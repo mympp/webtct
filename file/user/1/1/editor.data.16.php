ee15aff455b89f2a067da03a99bd1f544adf86cc
cthougth <314325027@qq.com>
2017-12-27 17:12:26 +0800

    2017-12-27

--- file/user/1/1/editor.data.16.php
+++ file/user/1/1/editor.data.16.php
@@ -1,33 +1,285 @@
-<?php exit;?>2017-10-30 13:51:35<p style="text-indent:32px;">
-	<span></span><span>西安胰岛素泵销商价格</span><span style="font-family:;">|</span>西安胰岛素泵公司推荐<span style="font-family:;">|</span>福泩供
-</p>
-<p style="text-indent:32px;">
-	购买任意一款胰岛素泵<span style="font-family:;">,</span>赠送同型号耗材，欢迎前来选购。
+<?php exit;?>2017-12-27 16:14:35<p style="text-align:center;">
+	<img src="http://image.135editor.com/files/users/382/3825395/201712/NEjDsKPC_xjyc.png" style="width:100%;height:auto !important;" width="100%" height="auto" border="0" title="" alt="" /> 
 </p>
 <p>
-	<span style="font-family:;"> </span>胰岛素泵进入中国已有<span style="font-family:;">15</span>年历史，然而目前国内个人长期用泵者仅有<span style="font-family:;">4</span>万例左右，在中国使用胰岛素泵的患者绝大多数为<span style="font-family:;">2</span>型糖尿病。一方面，部分使用胰岛素泵<span style="font-family:;">cure</span>的<span style="font-family:;">2</span>型糖尿病患者实际上是不需要胰岛素泵<span style="font-family:;">cure</span>的，其可能1天仅需注射一次胰岛素即可。但是，另一方面，还有一部分真正需要胰岛素泵<span style="font-family:;">cure</span>的<span style="font-family:;">1</span>型糖尿病患者却没有使用胰岛素泵。其限制因素可能有以下几点：起初，费用问题。目前胰岛素泵在我国未纳入报销范围。胰岛素泵的耗材费用很高，且需经常更换针头、管路，这些也不能报销，导致许多患者因为经济原因而放弃使用胰岛素泵；其次，使用胰岛素泵的患者需要相对严格的监测血糖，通过血糖水平来及时调整胰岛素剂量，达到更好的<span style="font-family:;">cure</span>效果。而监测血糖的费用目前也是不能报销的。这些费用限制了需要使用胰岛素泵<span style="font-family:;">cure</span>患者的选择；在国内正真掌握胰岛素泵<span style="font-family:;">cure</span>的医院和医生数量有限，这也是一个限制因素。
+	<img src="http://image.135editor.com/files/users/382/3825395/201712/vIrkmUx7_MFKE.png" alt="1.png" /> 
 </p>
-<p style="margin-left:0;text-indent:32px;">
-	<strong><span style="font-family:宋体;color:red;">官网网址：</span></strong><strong><span style="font-family:;"><span style="color:red;">gansufushengshangmao.shop.88360.com</span></span></strong> 
+<p style="font-size:14px;color:#A0A0A0;">
+	<span style="font-size:16px;color:#000000;"><strong>传统钢针输液的弊端</strong></span> 
 </p>
-<p style="margin-left:0;text-indent:32px;">
-	<strong><span style="font-family:;"> <span style="color:#FF0000;">www.forcen123.com</span><span style="color:#FF0000;"> </span> </span></strong> 
+<ul class=" list-paddingleft-2">
+	<li>
+		<p>
+			<span style="font-size:14px;color:#7F7F7F;">传统钢针易让人产生恐惧感，婴幼儿输液、扎针易痛哭；</span> 
+		</p>
+	</li>
+	<li>
+		<p style="font-size:14px;color:#A0A0A0;">
+			<span style="font-size:14px;color:#7F7F7F;">输液过程中若消毒不严、操作不规范，可引起交叉感染，每年全世界有数万病人遭受院内感染；</span> 
+		</p>
+	</li>
+	<li>
+		<p style="font-size:14px;color:#A0A0A0;">
+			<span style="font-size:14px;color:#7F7F7F;">输液过程中易发生滚针、鼓包、静脉炎等不良反应，因输液问题导致的医患纠纷占30%以上。</span> 
+		</p>
+		<p>
+			<br />
+		</p>
+		<p style="font-size:14px;color:#A0A0A0;">
+			<img src="http://image.135editor.com/files/users/382/3825395/201712/BF6O7EWM_7yxW.png" alt="3.png" /> 
+		</p>
+	</li>
+</ul>
+<p style="font-size:14px;color:#A0A0A0;">
+	<br />
 </p>
-<p style="margin-left:0;text-indent:32px;">
-	<strong><span style="font-family:宋体;color:red;">联系人：段女士</span></strong> 
+<span style="font-size:16px;"><strong class="135brush">对钢针输液说“NO”！</strong></span> 
+<ul class=" list-paddingleft-2">
+	<li>
+		<p>
+			<span style="font-size:14px;">2015年05月发表于《中国护理管理》——</span>软管输液产品全面取代钢针输液，才能真正全面提高护理质量！
+		</p>
+	</li>
+	<li>
+		<p>
+			<span style="font-size:14px;">国家【静脉输液指南】、</span> 
+		</p>
+		<p>
+			<span style="font-size:14px;"></span>国家行业标准-静脉治疗护理技术操作规范、
+		</p>
+		<p>
+			卫生部“十大静脉输液安全目标”等，
+		</p>
+		<p>
+			<span style="font-size:14px;"><strong>纷纷倡导对钢针输液“零容忍”！</strong></span> 
+		</p>
+	</li>
+</ul>
+<p>
+	<span style="color:#C00000;font-size:15px;"><strong> &nbsp;国家药监部门明确指出：<strong></strong></strong></span> 
 </p>
-<p style="margin-left:0;text-indent:32px;">
-	<span style="color:#FF0000;"><strong><span style="font-family:宋体;">手机</span></strong><strong>:&nbsp;15108387998</strong></span> 
+<p>
+	<span style="color:#C00000;font-size:15px;"><strong> &nbsp;对优秀的输液产品即安全输液产品，</strong></span> 
 </p>
-<p style="margin-left:0;text-indent:32px;">
-	<span style="color:#FF0000;"><strong><span style="color:red;font-family:宋体;">座机：</span></strong><strong>0931-2111959</strong></span> 
+<p>
+	<span style="color:#C00000;font-size:15px;"><strong> &nbsp;要鼓励和加快用于临床，以更有效地服务于患者健康！</strong></span> 
 </p>
-<p style="margin-left:0;text-indent:32px;">
-	<span style="color:#FF0000;"><strong>Q&nbsp;Q</strong><strong><span style="color:red;font-family:宋体;">：</span></strong><strong>1344456486</strong></span> 
+<br />
+<p>
+	<span style="font-size:18px;"><strong><span style="color:#FFFFFF;" class="135brush">《安输》—— 临床输液的更新换代产品</span></strong></span> 
 </p>
-<p style="margin-left:0;text-indent:32px;">
-	<span style="color:#FF0000;"><strong><span style="color:red;font-family:宋体;">邮箱：</span></strong><strong>Forcen123@163.com</strong></span> 
+<p>
+	<span style="color:#C00000;"><strong>主要部件均由知名企业定制加工</strong></span> 
 </p>
-<p style="margin-left:0;text-indent:32px;">
-	<span style="color:#FF0000;"><strong><span style="color:red;font-family:宋体;">地址：兰州市城关区通渭路</span></strong><strong>1</strong><strong><span style="color:red;font-family:宋体;">号房地产大厦</span></strong><strong>18</strong><strong><span style="color:red;font-family:宋体;">楼</span></strong><strong>1821</strong><strong><span style="color:red;font-family:宋体;">室</span></strong></span> 
+<ul class=" list-paddingleft-2">
+	<li>
+		<p>
+			<span style="font-size:15px;color:#000000;">前端侧孔针、铆钉为世界顶尖制造商日本NCC提供；</span> 
+		</p>
+	</li>
+	<li>
+		<p>
+			<span style="font-size:15px;color:#000000;">前端软管由世界顶尖医用导管供应商芬兰optimez供应，并有安全钡线；</span> 
+		</p>
+	</li>
+	<li>
+		<p>
+			<span style="font-size:15px;color:#000000;">硅胶垫精选德国原装进口食品级硅胶；</span> 
+		</p>
+	</li>
+	<li>
+		<p>
+			<span style="font-size:15px;color:#000000;">产品材质选用对人体无害的安全材质，其标准远超欧美标准。</span> 
+		</p>
+	</li>
+</ul>
+<p>
+	<img src="http://image.135editor.com/files/users/382/3825395/201712/Jv4V32U6_c4Vm.png" alt="4.png" /> 
+</p>
+<p>
+	<span style="color:#C00000;"><strong>型号齐全，全科通用</strong></span> 
+</p>
+<p>
+	<img src="http://image.135editor.com/files/users/382/3825395/201712/VCAfrzmS_b4fr.png" alt="5.png" /> 
+</p>
+<p>
+	<span style="color:#C00000;"><strong>一大创新，三大选择</strong></span> 
+</p>
+<ul class=" list-paddingleft-2">
+	<li>
+		<p>
+			<span style="font-size:15px;">《安输》集合了目前所有输液产品的优势于一身，也是国内外唯一一款软管输液产品；</span> 
+		</p>
+	</li>
+	<li>
+		<p>
+			<span style="font-size:15px;">《安输》完全避免了传统钢针输液器的种种弊端；</span> 
+		</p>
+	</li>
+	<li>
+		<p>
+			<span style="font-size:15px;">《安输》一体成型，完全避免了留置针和输液器在污染环境中对接致感染的风险，也大大减少了护理工作量；</span> 
+		</p>
+	</li>
+	<li>
+		<p>
+			<span style="font-size:15px;">《安输》使用不改变以往护士输液操作习惯，使用便捷、安全（留置针需要专业护士操作）。</span> 
+		</p>
+	</li>
+</ul>
+<p>
+	<br />
+</p>
+<strong class="135brush">全国市场，贴心服务</strong> 
+<p>
+	<span style="font-size:15px;color:#000000;"> &nbsp; &nbsp; &nbsp;《安输》被医学护理学专家评为近十年来最有价值的医疗器械产品，一上市即创造了数个奇迹：<br />
+</span> 
+</p>
+<p>
+	<span style="font-size:15px;color:#000000;">1、迅速被北京各大医院医护专家认可接受并进入临床；</span> 
+</p>
+<p>
+	<span style="font-size:15px;color:#000000;">2、迅速被上海医护专家评定为上海人民的医保产品。</span> 
+</p>
+<p>
+	<span style="font-size:15px;color:#000000;"><strong>市场服务</strong><br />
+</span> 
+</p>
+<ul class=" list-paddingleft-2">
+	<li>
+		<p>
+			<span style="font-size:15px;color:#000000;">北京、广州、上海、深圳、重庆、内蒙、珠海等地的重点医院纷纷引进使用；</span> 
+		</p>
+	</li>
+	<li>
+		<p>
+			<span style="font-size:15px;color:#000000;">2018年将全面进入全国各大医院使用。</span> 
+		</p>
+	</li>
+</ul>
+<p>
+	<span style="font-size:15px;color:#000000;"><strong>临床服务</strong></span> 
+</p>
+<ul class=" list-paddingleft-2">
+	<li>
+		<p>
+			<span style="font-size:15px;color:#000000;">全面提高医院护理质量，大幅降低医患纠纷；</span> 
+		</p>
+	</li>
+	<li>
+		<p>
+			<span style="font-size:15px;color:#000000;">大幅减少护士的工作量，减轻工作压力；</span> 
+		</p>
+	</li>
+	<li>
+		<p>
+			<span style="font-size:15px;color:#000000;">提高护士工作技术，护患关系更和谐。<br />
+</span> 
+		</p>
+	</li>
+</ul>
+<p>
+	<span style="font-size:15px;color:#000000;"><strong>学术支持</strong></span> 
+</p>
+<ul class=" list-paddingleft-2">
+	<li>
+		<p>
+			<span style="font-size:15px;color:#000000;">全新医疗是【软管输液系统】国家标准唯一起草单位；</span> 
+		</p>
+	</li>
+	<li>
+		<p>
+			<span style="background-color:#FAFAFA;font-size:15px;color:#000000;">中国非公临工护理学分委会主席单位。</span> 
+		</p>
+	</li>
+</ul>
+<p>
+	<br />
+</p>
+<strong>绝对的市场优势</strong> 
+<ul class=" list-paddingleft-2">
+	<li>
+		<p style="color:#3E3E3E;">
+			<span style="color:#000000;font-size:15px;"><strong>独家容易中标</strong></span> 
+		</p>
+		<p style="color:#3E3E3E;">
+			<span style="color:#000000;font-size:15px;"></span><span style="color:#000000;font-size:15px;">独家产品，易中标，同类产品中没有竞品竞争。</span> 
+		</p>
+	</li>
+	<li>
+		<p>
+			<span style="font-size:15px;"><strong>仿制必究</strong></span> 
+		</p>
+		<p>
+			<span style="font-size:15px;">数项专利保护；</span> 
+		</p>
+		<p>
+			<span style="font-size:15px;"></span><span style="font-size:15px;">三类《注册证》审批严格，审批周期三年以上。</span> 
+		</p>
+	</li>
+</ul>
+<p>
+	<br />
+</p>
+<span style="font-size:16px;"><strong>巨大的市场空间</strong></span> 
+<p style="color:#3E3E3E;">
+	<span style="font-size:15px;color:#000000;"> &nbsp;<span style="font-size:15px;color:#000000;"> &nbsp; &nbsp; 输液（器）系统是药物进入人体达到治病救人最主要、最快捷的途径！</span></span> 
+</p>
+<p style="color:#3E3E3E;">
+	<span style="font-size:15px;color:#000000;"> &nbsp; &nbsp; &nbsp;国家统计局于2016年统计，我国医疗输液产品消耗<span style="font-size:16px;color:#000000;"><strong>200亿</strong></span>套。超过<strong><span style="font-size:17px;color:#000000;">80%</span></strong>的病人在治疗期间，接收不同形式的静脉输液治疗，而且输液往往持续到治疗结束。<span style="font-size:17px;color:#000000;"><strong>输液产品有</strong><strong>2000亿元的巨大市场。</strong></span></span> 
+</p>
+<p>
+	<br />
+</p>
+<img title="烟花" src="http://image3.135editor.com/cache/remote/aHR0cHM6Ly9tbWJpei5xbG9nby5jbi9tbWJpei95cVZBcW9adkRpYkdNRUE4dnZ4Z0ZHWGxCdUtsQllHc0dNcEpDWWJkejkzWHR6NmhzdWtXQ0hreW9nYVhMc2NOVExOUnZWUTh5dnJlbzl5NzlEa0FZOHcvMD93eF9mbXQ9Z2lm" /> <span style="font-family:微软雅黑;font-size:16px;"><strong>证书展示</strong></span> 
+<p>
+	<img src="http://image.135editor.com/files/users/382/3825395/201712/ZI8L4nFJ_gTvf.png" alt="证书展示-截图.png" /> 
+</p>
+<p style="text-align:center;">
+	<img title="欧式古典 分割线" src="http://image2.135editor.com/cache/remote/aHR0cHM6Ly9tbWJpei5xcGljLmNuL21tYml6X3BuZy91TjFMSWF2N29KaWJJR2lhZXNRWUZaNVhNdlN0Z1hkSlJZdmhJV3J3R0ZmcHc1Znp1SFcxUGVGWVd0NWljQXlpYmZaaHFodGljNmhKczhVR1d5QXE3bmljR25SUS8wP3d4X2ZtdD1wbmc=" style="width:150px;" /> 
+</p>
+<p>
+	<strong><span style="color:#FFFFFF;" class="135brush">联系我们</span></strong> 
+</p>
+<p>
+	<span style="font-size:15px;">公司名称：合肥全新医疗设备有限公司</span> 
+</p>
+<p>
+	<span style="font-size:15px;">公司网址：<span style="text-decoration:none;color:#000000;font-size:15px;"><a href="http://www.chinamkl.com">www.chinamkl.com</a></span></span> 
+</p>
+<p>
+	<span style="color:#000000;font-size:15px;">全国招商联系人：</span> 
+</p>
+<p>
+	<span style="color:#0070C0;font-size:15px;">张先生：18620204932</span> 
+</p>
+<p>
+	<span style="color:#0070C0;font-size:15px;">丁先生：18911410767?</span> 
+</p>
+<p>
+	<span style="color:#0070C0;font-size:15px;">?刘先生：13701029787 / 18022398915</span> 
+</p>
+<p>
+	<span style="color:#C00000;font-size:18px;"><strong>招商会议信息</strong></span> 
+</p>
+<p style="text-align:center;">
+	<img src="http://image3.135editor.com/cache/remote/aHR0cHM6Ly9tbWJpei5xbG9nby5jbi9tbWJpei95cVZBcW9adkRpYkdSMHNmZmJHU3RUYjRVRzRBelNmME1LalMyVGhnSm03S0cyajk3NldMUVAweUoweUwzdWlhWFpmOTFPNktzM1VhMVBpYjJrc0dXTzRpYXcvMD93eF9mbXQ9cG5n" style="width:100%;" /> 
+</p>
+<p>
+	<strong><span style="font-size:15px;">时间：2018年1月15日</span></strong> 
+</p>
+<p>
+	<strong><span style="font-size:15px;">地点：浙江省嘉兴市</span></strong> 
+</p>
+<p>
+	<span style="color:#C00000;font-size:18px;"><strong>参会回执</strong></span> 
+</p>
+<p>
+	<br />
+</p>
+<p>
+	<br />
+</p>
+<p>
+	<br />
 </p>
